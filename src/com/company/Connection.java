5a3241c81bdfe841e1764f24857dd2a49a7264fd
universalevil <rita.tsurenko@gmail.com>
2016-09-14 09:09:28 -0400

    to commint Main

--- src/com/company/Connection.java
+++ src/com/company/Connection.java
@@ -4,4 +4,21 @@ package com.company;
  * Created by mtsurenko on 9/14/2016.
  */
 public class Connection {
+
+    //1.1.
+    Connection (int i)
+    {};
+
+    String str11 = "This is a field";
+
+    public void method11 () {
+
+        System.out.println ("printing what's in the field: " + str11);
+
+    };
+
+    public void method11 (String str11){
+        String str2 = "result of 2nd overloaded method";
+
+    };
 }
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by mtsurenko on 9/14/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ;
+
+        //1.1.
+        String result;
+        int j=1;
+        Connection inv = new Connection(j);
+
+    }
+
+}
